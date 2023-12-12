9dfaa3898b26421453c34d8359f5e91654d76d25
Jorge Amat Soler <amjsoler@gmail.com>
2023-12-12 10:19:20 +0100

    Ahora Los jobs de esfiado se ejecutan cada 30 mins

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,8 +15,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         //TODO: Una vez en produccion, ver si estas tareas requieren de un withoutoverlapping
-        $schedule->job(new EsnifarSorteos())->everyTenMinutes();
-        $schedule->job(new EsnifarResultados())->everyTenMinutes();
+        $schedule->job(new EsnifarSorteos())->everyThirtyMinutes();
+        $schedule->job(new EsnifarResultados())->everyThirtyMinutes();
     }
 
     /**
